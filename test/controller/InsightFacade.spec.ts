146125d8a23d59fe98e1a114ca9b4653da0076e1
Mohan Grewal <mohangrewal24@gmail.com>
2021-09-12 09:13:47 -0700

    Added zip files for testing

--- test/controller/InsightFacade.spec.ts
+++ test/controller/InsightFacade.spec.ts
@@ -1,6 +1,7 @@
 import {InsightDatasetKind} from "../../src/controller/IInsightFacade";
 import InsightFacade from "../../src/controller/InsightFacade";
 import {expect} from "chai";
+import * as JSZip from "jszip";
 
 describe("InsightFacade", function() {
     describe("addDataset", function() {
@@ -27,12 +28,13 @@ describe("InsightFacade", function() {
             });
         })
         it("should return one currently added dataset", function() {
-            insightFacade.addDataset("BIO112", "test", InsightDatasetKind.Courses);
+            insightFacade.addDataset("ubc",
+                convertToBase64("test/resources/archives/Dataset1/courses.zip"), InsightDatasetKind.Courses);
             insightFacade.listDatasets().then((result) => {
                 expect(result.length).to.equals(1);
 
-                expect(result[0].id).to.equals("BIO112");
-                expect(result[0].numRows).to.equals(1);
+                expect(result[0].id).to.equals("ubc");
+                //expect(result[0].numRows).to.equals(1);
                 expect(result[0].kind).to.equals(InsightDatasetKind.Courses);
                 return;
             }, expect.fail("Failed to Return One Dataset"));
@@ -40,18 +42,20 @@ describe("InsightFacade", function() {
         });
 
         it("should return multiple currently added datasets",  function() {
-            insightFacade.addDataset("BIO112", "test", InsightDatasetKind.Courses);
-            insightFacade.addDataset("ANTH317A", "test2", InsightDatasetKind.Courses);
+            insightFacade.addDataset("ubc",
+                convertToBase64("test/resources/archives/Dataset1/courses.zip"), InsightDatasetKind.Courses);
+            insightFacade.addDataset("science",
+                convertToBase64("test/resources/archives/Dataset2/courses.zip"), InsightDatasetKind.Courses);
 
             insightFacade.listDatasets().then((result) => {
                 expect(result.length).to.equals(2);
 
-                expect(result[0].id).to.equals("BIO112");
-                expect(result[0].numRows).to.equals(1);
+                expect(result[0].id).to.equals("ubc");
+                //expect(result[0].numRows).to.equals(1);
                 expect(result[0].kind).to.equals(InsightDatasetKind.Courses);
 
-                expect(result[1].id).to.equals("ANTH317A");
-                expect(result[1].numRows).to.equals(1);
+                expect(result[1].id).to.equals("science");
+                //expect(result[1].numRows).to.equals(1);
                 expect(result[1].kind).to.equals(InsightDatasetKind.Courses);
                 return;
             }, expect.fail("Failed to Return Multiple Datasets"));
@@ -59,3 +63,11 @@ describe("InsightFacade", function() {
 
     });
 });
+
+//Citation: https://stackoverflow.com/questions/28834835/readfile-in-base64-nodejs
+function convertToBase64(filePath: string) : string {
+    const fs = require('fs');
+    return fs.readFileSync(filePath, {encoding: 'base64'});
+}
+
+
