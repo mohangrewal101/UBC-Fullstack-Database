955e0e7c253cc85b312f6bee111caf76396236a6
Mohan Grewal <mohangrewal24@gmail.com>
2021-09-11 17:43:05 -0700

    Added Promises to listDatasets test

--- test/controller/InsightFacade.spec.ts
+++ test/controller/InsightFacade.spec.ts
@@ -20,32 +20,34 @@ describe("InsightFacade", function() {
             insightFacade = new InsightFacade();
         });
 
-        it("should return one currently added dataset", async function() {
+        it("should return one currently added dataset", function() {
             insightFacade.addDataset("BIO112", "test", InsightDatasetKind.Courses);
-            const result = await insightFacade.listDatasets();
+            insightFacade.listDatasets().then((result) => {
+                expect(result.length).to.deep.equals(1);
 
-            expect(result.length).to.equals(1);
+                expect(result[0].id).to.deep.equals("BIO112");
+                expect(result[0].numRows).to.deep.equals(1);
+                expect(result[0].kind).to.deep.equals(InsightDatasetKind.Courses);
 
-            expect(result[0].id).to.equals("BIO112");
-            expect(result[0].numRows).to.equals(1);
-            expect(result[0].kind).to.equals(InsightDatasetKind.Courses);
+            })
 
         });
 
-        it("should return multiple currently added datasets", async function() {
+        it("should return multiple currently added datasets",  function() {
             insightFacade.addDataset("BIO112", "test", InsightDatasetKind.Courses);
             insightFacade.addDataset("ANTH317A", "test2", InsightDatasetKind.Courses);
-            const result = await insightFacade.listDatasets();
 
-            expect(result.length).to.equals(2);
+            insightFacade.listDatasets().then((result) => {
+                expect(result.length).to.equals(2);
 
-            expect(result[0].id).to.equals("BIO112");
-            expect(result[0].numRows).to.equals(1);
-            expect(result[0].kind).to.equals(InsightDatasetKind.Courses);
+                expect(result[0].id).to.equals("BIO112");
+                expect(result[0].numRows).to.equals(1);
+                expect(result[0].kind).to.equals(InsightDatasetKind.Courses);
 
-            expect(result[1].id).to.equals("ANTH317A");
-            expect(result[1].numRows).to.equals(1);
-            expect(result[1].kind).to.equals(InsightDatasetKind.Courses);
+                expect(result[1].id).to.equals("ANTH317A");
+                expect(result[1].numRows).to.equals(1);
+                expect(result[1].kind).to.equals(InsightDatasetKind.Courses);
+            });
         });
 
     });
