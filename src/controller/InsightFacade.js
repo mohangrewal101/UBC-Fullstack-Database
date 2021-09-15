a510adc2e614c2fe300e0c214e5e7c2ec03e2281
Mohan Grewal <mohangrewal24@gmail.com>
2021-09-15 09:03:15 -0700

    New commit for autobot testing

--- src/controller/InsightFacade.js
+++ src/controller/InsightFacade.js
@@ -1,9 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const IInsightFacade_1 = require("./IInsightFacade");
 class InsightFacade {
     addDataset(id, content, kind) {
-        return Promise.reject(new IInsightFacade_1.InsightError());
+        return Promise.reject();
     }
     removeDataset(id) {
         return Promise.reject();
